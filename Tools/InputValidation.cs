d716f4acdeda9dfaf13d0878b53aa128020fcd92
Tom Bell <tbell@tombellnj.com>
2014-11-08 09:06:35 -0500

    intial commit

---
+++ Tools/InputValidation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Tools {
+    public class InputValidation {
+        public static bool checkLetters(string input) {
+            int count = Regex.Matches(input, @"[a-zA-Z\-\ ]").Count;
+
+            if (input.Length != 0 && count == input.Length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool checkNumbers(string input) {
+            int count = Regex.Matches(input, @"[0-9]").Count;
+
+            if (input.Length != 0 && count == input.Length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public static bool checkDecimals(string input){
+            int count = Regex.Matches(input, @"[0-9\.]").Count;
+
+            if (input.Length != 0 && count == input.Length) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
