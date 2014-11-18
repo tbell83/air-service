cc24a0e3cbe6aaaac0bbf118b877b11f71674a2f
Tom Bell <tbell@tombellnj.com>
2014-11-18 16:15:13 -0500

    class for password hashing, validation

---
+++ TermProject/password.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Security.Cryptography;
+using System.Globalization;
+
+namespace TermProject{
+    public static class password{
+
+        public static string hashPassword(SHA512 sha512Hash, string password){
+            byte[] data = sha512Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+            StringBuilder hashedPassword = new StringBuilder();
+            for (int i = 0;i<data.Length;i++){
+                hashedPassword.Append(data[i].ToString("x2"));
+            }
+            return hashedPassword.ToString();
+        }
+
+        public static bool checkPassword(SHA512 sha512Hash, string password, string hashedPassword){
+            if(hashPassword(sha512Hash, password) == hashedPassword){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
+}
