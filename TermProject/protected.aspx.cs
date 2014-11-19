a91d89937a3b658040a3bd3feb8dac154cff54a7
Tom Bell <tbell@tombellnj.com>
2014-11-19 07:25:12 -0500

    Final push for login

--- TermProject/protected.aspx.cs
+++ TermProject/protected.aspx.cs
@@ -12,7 +12,12 @@ namespace TermProject
         protected void Page_Load(object sender, EventArgs e)
         {
             if(Session["user"] == null){
+                unauthed.Visible=true;
+                Response.AddHeader("REFRESH","50;URL=login.aspx");
                 Response.Redirect("login.aspx");
+            }else{
+                authed.Visible=true;
+                lblUsername.Text = "WELCOME BACK, " + Session["user"].ToString() + "!";
             }
         }
     }
