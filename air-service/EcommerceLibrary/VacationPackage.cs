7d4943f5a77ec9f7b5361f4bae15c96ac519375f
Lisa Clark <lisa.clark@temple.edu>
2014-12-01 16:27:34 -0500

    switched vacationpackage class to a class libary

--- air-service/EcommerceLibrary/VacationPackage.cs
+++ air-service/EcommerceLibrary/VacationPackage.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 using System.Collections;
 
-namespace TermProject
+namespace EcommerceLibrary
 {
     public class VacationPackage
     {
